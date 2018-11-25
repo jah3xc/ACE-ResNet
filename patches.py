22c9b443c4d6491ab8924c3579a1b8f43dbe50c8
James Hurt <jah3xc@mail.missouri.edu>
2018-11-24 21:43:00 -0600

    keep

--- patches.py
+++ patches.py
@@ -57,7 +57,7 @@ def extract_patches(data, ground_truth, window_size, stride, maxPatches = None):
     patches = patches[1:]
     # convert to numpy categorical
     labels = np.array(labels)
-    labels = keras.utils.to_categorical(labels)
+    # labels = keras.utils.to_categorical(labels)
     logger.debug("Shape of Labels: {}".format(labels.shape))
     logger.info("Number of patches found: {}".format(len(patches)))
 
